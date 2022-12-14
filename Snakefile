d8f37fe92ce0e1037d391eeb437c967e5cf5e7e3
Jon Gauthier <jon@gauthiers.net>
2022-12-13 21:36:10 -0500

    prepare evaluate_with_prefixes

--- Snakefile
+++ Snakefile
@@ -13,6 +13,7 @@ rule all:
                model=config["models"], prefix_type=["grammatical", "ungrammatical"],
                suite=config["suites"], prefix_suite=config["suites"])
 
+
 rule evaluate_prefixes:
     resources:
         mem_mb = 10000
@@ -32,6 +33,39 @@ rule evaluate_prefixes:
             --prefix_type {wildcards.prefix_type} \
             -o {output} \
             --target-length {config[prefixing][target_length]} \
+            --target-size {config[prefixing][target_size]} \
+            --model-id {wildcards.model} \
+            || echo "core dump probably"
+        '
+
+        # Remove dumped core :(
+        rm -f core*
+        """
+
+
+rule evaluate_fixed_prefixes:
+    resources:
+        mem_mb = 10000
+
+    input:
+        prefix_file = "prefixes/{prefix_source}.json"
+
+    output:
+        directory("output/{model}/fixed/{suite}/{prefix_source}")
+
+    shell:
+        """
+        mkdir -p {output}
+
+        bash -c '
+            . $HOME/.bashrc # if not loaded automatically
+            conda activate huggingface
+        python evaluate_with_prefixes.py \
+            --suite {wildcards.suite} \
+            --prefix_file {input.prefix_file} \
+            --prefix_label {wildcards.prefix_source} \
+            -o {output} \
+            --target-lengths 20,100,200,300,400,500,600 \
             --target-size {config[prefixing][target_size]} \
             --model-id {wildcards.model} \
             || echo "core dump probably"
--- evaluate_with_prefixes.py
+++ evaluate_with_prefixes.py
@@ -5,7 +5,7 @@ Run SyntaxGym evaluation with arbitrary prefix data.
 
 from argparse import ArgumentParser
 from copy import deepcopy
-import itertools
+import json
 from pathlib import Path
 import re
 from readline import get_history_length
@@ -33,75 +33,50 @@ def regions_to_string(regions):
 ItemNumber = int
 ConditionName = str
 
-def get_prefix_sentences(suite: datasets.Dataset,
-                         prefix_conditions: List[str],
-                         ) -> Tuple[np.ndarray,
-                                     List[Tuple[ItemNumber, ConditionName, str]],
-                                     np.ndarray]:
-    # Retrieve all sentences from the given `prefix_conditions`
-    # that could participate in prefix.
-    prefix_sentences = []
-    prefix_sentence_item_numbers = []
-    for item in suite:
-        sentence_map = dict(zip(item["conditions"]["condition_name"],
-                                item["conditions"]["content"]))
-        for cond in prefix_conditions:
-            prefix_sentences.append((item["item_number"], cond, sentence_map[cond]))
-            prefix_sentence_item_numbers.append(item["item_number"])
-
-    prefix_sentence_item_numbers = np.array(prefix_sentence_item_numbers)
-    prefix_sentence_lengths = np.array([
-        sentence.count(" ") + 1 for _, _, sentence in prefix_sentences])
-
-    return (prefix_sentence_item_numbers,
-            prefix_sentences,
-            prefix_sentence_lengths)
-
 
 def expand_suite(suite: datasets.Dataset,
-                 target_length: int,
-                 prefix_list: List[str],
+                 prefixes: Dict[int, List[str]],
+                 target_lengths: List[int],
                  target_size=1000):
     """
-    Expand the examples in an item to contain prefixes consisting of sentence(s from
-    `prefix_list`.
+    Expand the examples in an item to contain prefixes consisting of prefix strings from
+    `prefix_data` (which are grouped by approximate length in tokens).
     Expand until all inputs under `target_length` tokens (based on whitespace split) are
     generated.)
 
     Adds a single condition to the item storing the entire prefix content.
     """
 
-    expanding_self = False
-    assert other_prefix_conditions is not None, "Must specify prefix conditions for other suite."
+    size_per_length = target_size // len(target_lengths)
 
-    # Rough prefix token count
-    pr_lengths = np.array([prefix.count(" ") + 1 for prefix in prefix_list])
+    results: List[Tuple[int, int, int]] = []
 
-    # About many prefixes will we need to reach target_length?
-    num_prefixes = int(np.ceil(target_length / pr_lengths.mean()))
-    num_samples_per_prefix = int(np.ceil(target_size / num_prefixes))
+    for target_length in target_lengths:
+        if size_per_length > len(prefixes[target_length]):
+            raise ValueError(f"Not enough prefixes at length {target_length} "
+                             f"({len(prefixes[target_length])}) to generate "
+                             f"unique items (target size is {size_per_length}). "
+                              "Reduce target size or add prefixes.")
 
-    results = []
-    for num_prefixes_i in trange(1, num_prefixes, desc="Prefix lengths"):
-        # Sample sentences to use in this prefix.
+        # Sample items to use
         item_sample = np.random.choice(
             range(len(suite)),
-            size=num_samples_per_prefix,
+            size=size_per_length,
             replace=True)
 
-        for item_idx in item_sample:
-            # Sample prefixes.
-            prefix_sample = np.random.choice(
-                len(prefix_list),
-                size=num_prefixes_i,
-                replace=False)
+        # Sample prefixes to use
+        prefix_sample = np.random.choice(len(prefixes[target_length]),
+                                         size=size_per_length,
+                                         replace=False)
 
-            results.append((prefix_sample, int(item_idx)))
+        results.extend(list(zip(np.repeat(target_length, len(prefix_sample)),
+                                prefix_sample.tolist(), item_sample.tolist())))
 
     #########
 
     # Update existing dataset with null data.
     suite = suite.add_column("prefix_length", [0] * len(suite))
+    suite = suite.add_column("prefix_source", [""] * len(suite))
 
     def add_empty_region(item):
         """
@@ -132,18 +107,17 @@ def expand_suite(suite: datasets.Dataset,
 
     acc = len(suite)
     new_items = []
-    for prefixes, item_idx in tqdm(results, desc="Generating items"):
+    for prefix_length, prefix_idx, item_idx in tqdm(results, desc="Generating items"):
         item = deepcopy(suite[item_idx])
         item["item_number"] = acc + 1
         acc += 1
 
-        # TODO do they already contain punctuation? I think so.
-        # prefix_str = ". ".join([prefix_list[idx] for idx in prefixes]) + "."
-        prefix_str = " ".join([prefix_list[idx] for idx in prefixes])
+        prefix_str = prefixes[prefix_length][prefix_idx]
         for cond_regions in item["conditions"]["regions"]:
             cond_regions["content"][0] = prefix_str
 
         item["prefix_length"] = prefix_str.count(" ") + 1
+        item["prefix_source"] = f"{prefix_length}/{prefix_idx}"
         item["conditions"]["content"] = [
             regions_to_string(regions) for regions in item["conditions"]["regions"]]
 
@@ -185,16 +159,29 @@ def main(args):
         }
     }
 
-    conditions_suite = suite_conditions[args.prefix_type][args.suite.split("_")[0]]
+    prefix_label = args.prefix_file.stem if args.prefix_label is None else args.prefix_label
+    with args.prefix_file.open() as prefix_f:
+        prefixes: Dict[int, List[str]] = {
+            int(prefix_length): sentences
+            for prefix_length, sentences in json.load(prefix_f).items()
+        }
+        
+        # Fix detokenization issue -- leftover @,@ , @.@, etc.
+        # from Moses tokenizer, not caught by detokenizer
+        moses_punct_escape_re = re.compile(r"\s+@([^@]+)@\s+")
+        prefixes = {
+            prefix_length: [moses_punct_escape_re.sub(r"\1", sentence) for sentence in sentences]
+            for prefix_length, sentences in prefixes.items()
+        }
 
-    # TODO
-    prefix_list = ...
+    target_lengths = [int(x) for x in args.target_lengths.split(",")] \
+        if args.target_lengths is not None else prefixes.keys()
 
-    expanded = expand_suite(suite, args.target_length, prefix_list,
+    expanded = expand_suite(suite, prefixes, target_lengths=target_lengths,
                             target_size=args.target_size)
 
     # The input to the metric needs to match the expected feature spec.
-    expanded_input = expanded.map(remove_columns=["prefix_length"])
+    expanded_input = expanded.map(remove_columns=["prefix_length", "prefix_source"])
 
     metric = evaluate.load("cpllab/syntaxgym")
     result = metric.compute(dataset=expanded_input, model_id=args.model_id,
@@ -204,7 +191,7 @@ def main(args):
         result.prediction_results,
         columns=[f"prediction_{i}" for i in range(len(result.prediction_results[0]))])
     prediction_df["prefix_length"] = expanded["prefix_length"]
-    prediction_df["prefix_label"] = args.prefix_label
+    prediction_df["prefix_label"] = prefix_label
     prediction_df.index = expanded["item_number"]
     prediction_df.index.name = "item_number"
     prediction_df.to_csv(args.output_dir / "predictions.csv")
@@ -214,7 +201,7 @@ def main(args):
     regions_df.index.name = "item_number"
     regions_df = regions_df.reset_index().melt(id_vars=["item_number"])
     regions_df["condition"], regions_df["region_number"] = regions_df["variable"].str
-    regions_df["prefix_label"] = args.prefix_label
+    regions_df["prefix_label"] = prefix_label
     regions_df.drop("variable", axis=1, inplace=True)
     regions_df.to_csv(args.output_dir / "regions.csv", index=False)
 
@@ -222,8 +209,9 @@ def main(args):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--suite", default="number_prep")
+    parser.add_argument("--prefix_file", type=Path, required=True)
     parser.add_argument("--prefix_label")
-    parser.add_argument("--target-length", type=int, default=40)
+    parser.add_argument("--target-lengths", type=str, help="comma-separated list of target lengths")
     parser.add_argument("--target-size", type=int, default=1000)
     parser.add_argument("-m", "--model-id", default="gpt2")
     parser.add_argument("-o", "--output_dir", type=Path, required=True)
